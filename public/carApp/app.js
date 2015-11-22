2dda87e3575fd81fa0d259a1f3733327d00a7f82
Robin <robinswaylu@gmail.com>
2015-11-21 20:52:33 -0800

    change the name of car app

--- public/carApp/app.js
+++ public/carApp/app.js
@@ -18,7 +18,7 @@
    
 
   var gems = [{
-      name: 'Mazda 3 from Mazda Los Angeles, CA 90007 ',
+      name: '2016 Mazda 3 ',
       description: "Purposeful styling, fuel-efficient engines and an ideal ride and handling balance keep the 2015 Mazda 3 among our favorites in the compact car class.",
       cool: 184,
       price: 17845,
@@ -32,7 +32,7 @@
       ],
       reviews: []
     }, {
-      name: 'i3 from BMW Santa Babara, CA 90567',
+      name: '2016 BMW i3',
       description: "For electric vehicle shoppers desiring some sportiness and luxury-brand panache, the 2015 BMW i3 is a very compelling choice. That the i3 is also surprisingly practical only furthers its appeal.",
       cool: 170,
       price: 42400,
@@ -46,7 +46,7 @@
       ],
       reviews: []
     }, {
-      name: 'Model S from Tesla Berkeley, CA 94753',
+      name: 'Tesla Model S',
       description: "As both the ultimate eco-friendly luxury car and an absurdly fast sports sedan, we simply love the Model S. With an aptly named “Ludicrous Mode,” the top-spec, all-wheel-drive P90D hits 60 mph in 2.8 seconds. And yes, the optional Autopilot can do the driving for you in certain situations. The other trim levels offer varying performance and range as you move down the lineup, but all offer a classy cabin and electrifying driving experience. We gave the Model 70 and 70D a 10Best award for 2016.",
       cool: '329 to 691',
       price: 71100,
