59933616fc678164d7649ea18f01ba3b34c27d8f
Robin <robinswaylu@gmail.com>
2016-11-18 16:08:15 -0800

    npm favicon

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var nodemailer = require("nodemailer");
+var express = require('express'); 
+//var nodemailer = require("nodemailer"); 
+var favicon = require('serve-favicon');
 var app = express();
 /*
 Here we are configuring our SMTP Server details.
 STMP is mail server which is responsible for sending and recieving email.
 */
  
-var transporter = nodemailer.createTransport({
-    service: "sendgrid",
-    auth: {
-        user: "robinswaylu",
-        pass: "Ahs@8:00"
-    }
-});
+//var transporter = nodemailer.createTransport({
+//    service: "sendgrid",
+//    auth: {
+//        user: "robinswaylu",
+//        pass: "Ahs@8:00"
+//    }
+//});
 /*------------------SMTP Over-----------------------------*/
 
 /*------------------Routing Started ------------------------*/
@@ -21,28 +22,28 @@ app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-app.get('/send', function (req, res) {
-    var mailOptions = {
-        from: 'robinswaylu@gmail.com',
-        to: 'robinswaylu@gmail.com',
-        subject: req.query.subject,
-        text: req.query.text
-    }
-    console.log(mailOptions);
+//app.get('/send', function (req, res) {
+//    var mailOptions = {
+//        from: 'robinswaylu@gmail.com',
+//        to: 'robinswaylu@gmail.com',
+//        subject: req.query.subject,
+//        text: req.query.text
+//    }
+//    console.log(mailOptions);
+//
+//    transporter.sendMail(mailOptions, function (error, response) {
+//        if (error) {
+//            console.log(error);
+//            res.end("error");
+//        } else {
+//            console.log("Message sent: " + response.message);
+//            res.end("sent");
+//        }
+//    });
+//});
 
-    transporter.sendMail(mailOptions, function (error, response) {
-        if (error) {
-            console.log(error);
-            res.end("error");
-        } else {
-            console.log("Message sent: " + response.message);
-            res.end("sent");
-        }
-    });
-});
-
-/*--------------------Routing Over----------------------------*/
-
+/*--------------------Routing Over----------------------------*/ 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use('/public', express.static(__dirname + '/public')); 
 app.listen(3000, function () {
     console.log("Express Started on Port 3000");
